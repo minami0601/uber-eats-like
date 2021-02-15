01a8b12adb97313a82aef3be28508b30d2910b6b
minami0601 <minami.love.bb@icloud.com>
2021-02-15 10:55:57 +0900

    create front

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+// apis
+import { fetchRestaurants } from '../apis/restaurants'; //←この行を追加する
+
 
 export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
   return (
     <Fragment>
       レストラン一覧

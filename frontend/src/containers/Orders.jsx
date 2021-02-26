d417e657dd6cd010438a4a0215832932de3af336
minami0601 <minami.love.bb@icloud.com>
2021-02-26 23:04:11 +0900

    仮注文詳細のAPIを叩く

--- frontend/src/containers/Orders.jsx
+++ frontend/src/containers/Orders.jsx
@@ -1,19 +1,33 @@
 // --- 次でuseEffectを追加 ---
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 
 // --- ここから追加 ---
 import { fetchLineFoods } from '../apis/line_foods';
 // --- ここまで追加 ---
-
+// reducers
+import {
+  initialState,
+  lineFoodsActionTyps,
+  lineFoodsReducer,
+} from '../reducers/lineFoods';
+// --- ここまで追加 ---
 export const Orders = () => {
-
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
 // --- ここから追加 ---
   useEffect(() => {
+    // --- ここから修正 ---
+    dispatch({ type: lineFoodsActionTyps.FETCHING });
     fetchLineFoods()
       .then((data) =>
-        console.log(data)
+        dispatch({
+          type: lineFoodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data
+          }
+        })
       )
       .catch((e) => console.error(e));
+    // --- ここから修正 ---
   }, []);
 // --- ここまで追加 ---
 
